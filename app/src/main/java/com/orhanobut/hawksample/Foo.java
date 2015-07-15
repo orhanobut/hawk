b5da999f529681912053b0d5b4175cf5fb52566d
orhanobut <orhanobut@gmail.com>
2015-07-15 10:07:05 +0200

    rx support

--- app/src/main/java/com/orhanobut/hawksample/Foo.java
+++ app/src/main/java/com/orhanobut/hawksample/Foo.java
@@ -4,11 +4,20 @@ package com.orhanobut.hawksample;
  * @author Orhan Obut
  */
 public class Foo {
-    int a = 1;
-    int b = 3;
-    String c = "asdfasdf";
+  int a = 1;
+  int b = 3;
+  String c = "asdfasdf";
 
-    void test() {
+  void test() {
 
-    }
+  }
+
+  @Override
+  public String toString() {
+    return "Foo{" +
+        "a=" + a +
+        ", b=" + b +
+        ", c='" + c + '\'' +
+        '}';
+  }
 }
---
+++ hawk/src/androidTest/java/com/orhanobut/hawk/HawkRxTest.java
@@ -0,0 +1,65 @@
+package com.orhanobut.hawk;
+
+import android.content.Context;
+import android.test.InstrumentationTestCase;
+import android.util.Log;
+
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+/**
+ * @author Orhan Obut
+ */
+public class HawkRxTest extends InstrumentationTestCase {
+
+  Context context;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    System.setProperty(
+        "dexmaker.dexcache",
+        getInstrumentation().getTargetContext().getCacheDir().getPath());
+    context = getInstrumentation().getContext();
+    Hawk.init(context, "testPassword");
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+    context = null;
+    Hawk.clear();
+    Hawk.resetCrypto();
+  }
+
+  private static final String KEY = "TAG";
+
+  public void testString() throws Exception {
+    Hawk.put(KEY, "hawk");
+
+    Hawk.<String>getObservable(KEY)
+        .observeOn(Schedulers.io())
+        .subscribeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Subscriber<String>() {
+          @Override
+          public void onCompleted() {
+            Log.d("rxtest", "completed");
+            assertTrue(true);
+          }
+
+          @Override
+          public void onError(Throwable e) {
+            Log.d("rxtest", "error");
+            assertTrue(false);
+          }
+
+          @Override
+          public void onNext(String s) {
+            Log.d("rxtest", s);
+            assertTrue(true);
+          }
+        });
+  }
+
+}
