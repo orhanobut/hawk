cf52ec10b61cb6abcfb7fbca549cce656d9741c8
Orhan Obut <oobut@atlassian.com>
2018-04-02 20:04:50 +1000

    Update javadocs to add missing information

--- hawk/src/main/java/com/orhanobut/hawk/Converter.java
+++ hawk/src/main/java/com/orhanobut/hawk/Converter.java
@@ -1,12 +1,17 @@
 package com.orhanobut.hawk;
 
-interface Converter {
+/**
+ * Used to handle encoding and decoding as an intermediate layer.
+ * <p>Implement this interface if a custom implementation is needed</p>
+ *
+ * @see HawkConverter
+ */
+public interface Converter {
 
   /**
    * Encodes the value
    *
    * @param value will be encoded
-   *
    * @return the encoded string
    */
   <T> String toString(T value);
@@ -15,9 +20,7 @@ interface Converter {
    * Decodes
    *
    * @param value is the encoded data
-   *
    * @return the plain value
-   *
    * @throws Exception
    */
   <T> T fromString(String value, DataInfo dataInfo) throws Exception;
